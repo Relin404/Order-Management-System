9e32cdb890cd57a78d4b4a9145051e434a17a9ea
relin404 <ahmadmashaal01@gmail.com>
2024-06-22 14:43:45 +0300

    chore: update UsersModule providers and exports

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { CartModule } from 'src/modules/carts/cart.module';
+import { OrdersModule } from 'src/modules/orders/orders.module';
 import { UsersController } from 'src/modules/users/users.controller';
 import { UsersRepository } from 'src/modules/users/users.repository';
 import { UsersService } from 'src/modules/users/users.service';
 
 @Module({
+  imports: [CartModule, OrdersModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
-  exports: [UsersRepository],
+  exports: [UsersService, UsersRepository],
 })
 export class UsersModule {}
