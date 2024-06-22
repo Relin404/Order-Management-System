8f951faa59f821d737e6c707a3eafb92c087ce97
relin404 <ahmadmashaal01@gmail.com>
2024-06-22 16:23:20 +0300

    docs: add Swagger documentation to UsersController

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -13,9 +13,9 @@ import { UpdateUserDto } from 'src/modules/users/dtos/update-user.dto';
 import { UsersService } from 'src/modules/users/users.service';
 import {
   ApiBody,
-  ApiConflictResponse,
   ApiNotFoundResponse,
   ApiOperation,
+  ApiParam,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
@@ -26,28 +26,34 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @ApiResponse({ status: 200, description: 'Users found' })
-  @ApiOperation({ summary: 'Public' })
+  @ApiOperation({ summary: 'Find all users', operationId: 'findAll' })
   @Get()
   async findAll() {
     return await this.usersService.findAll();
   }
 
+  @ApiOperation({
+    summary: 'Get user orders history',
+    operationId: 'getUserOrdersHistory',
+  })
+  @ApiParam({ name: 'userId', type: 'number' })
   @ApiResponse({ status: 200, description: 'Orders history found' })
-  @ApiNotFoundResponse({ description: 'Orders history not found' })
-  @ApiOperation({ summary: 'Public' })
+  @ApiNotFoundResponse({ description: 'User not found' })
   @Get(':userId/orders')
   async getUserOrdersHistory(@Param('userId', ParseIntPipe) userId: number) {
     return await this.usersService.getUserOrdersHistory(userId);
   }
 
+  @ApiParam({ name: 'id', type: 'number' })
   @ApiResponse({ status: 200, description: 'User found' })
   @ApiNotFoundResponse({ description: 'User not found' })
-  @ApiOperation({ summary: 'Public' })
+  @ApiOperation({ summary: 'Get user by ID', operationId: 'getUser' })
   @Get(':id')
   async getUser(@Param('id', ParseIntPipe) id: number) {
     return await this.usersService.findOneById(id);
   }
 
+  @ApiParam({ name: 'id', type: 'number' })
   @ApiBody({ type: UpdateUserDto, description: 'User data' })
   @ApiResponse({ status: 200, description: 'User updated' })
   @ApiNotFoundResponse({ description: 'User not found' })
@@ -59,6 +65,7 @@ export class UsersController {
     return await this.usersService.update(id, updateUserDto);
   }
 
+  @ApiParam({ name: 'id', type: 'number' })
   @ApiResponse({ status: 200, description: 'User deleted' })
   @ApiNotFoundResponse({ description: 'User not found' })
   @Delete(':id')
