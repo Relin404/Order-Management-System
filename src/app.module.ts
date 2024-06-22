1719bc013094c1ee7ca5c536c5e4dc1736110f90
relin404 <ahmadmashaal01@gmail.com>
2024-06-22 20:53:25 +0300

    feat: load all modules in AppModules imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from 'src/modules/users/users.module';
 import { CartItemsModule } from './modules/cart-items/cart-items.module';
 import { OrderItemsModule } from './modules/order-items/order-items.module';
@@ -8,18 +6,22 @@ import { ProductsModule } from 'src/modules/products/products.module';
 import { CartModule } from 'src/modules/carts/cart.module';
 import { OrdersModule } from 'src/modules/orders/orders.module';
 import { AuthModule } from 'src/modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'src/modules/prisma/prisma.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     AuthModule,
     CartModule,
     CartItemsModule,
     OrdersModule,
     OrderItemsModule,
+    PrismaModule,
     ProductsModule,
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
