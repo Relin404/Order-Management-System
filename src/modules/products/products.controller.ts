e4e2b3ee8c0e752114886e0511f8ccb264f9d60b
relin404 <ahmadmashaal01@gmail.com>
2024-06-22 15:52:34 +0300

    feat: add Swagger decorators and error handling to ProductsController

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -3,43 +3,72 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
 } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiConflictResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateProductDto } from 'src/modules/products/dtos/create-product.dto';
 import { UpdateProductDto } from 'src/modules/products/dtos/update-product.dto';
 import { ProductsService } from 'src/modules/products/products.service';
 
-@Controller()
+@ApiTags('Products')
+@Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @ApiBody({ type: CreateProductDto, description: 'Product data' })
+  @ApiResponse({ status: 201, description: 'Product created' })
+  @ApiConflictResponse({ description: 'Product already exists' })
+  @HttpCode(201)
   @Post()
   async create(@Body() createProductDto: CreateProductDto) {
     return await this.productsService.create(createProductDto);
   }
 
+  @ApiOperation({ summary: 'Find all products', operationId: 'findAll' })
+  @ApiResponse({ status: 200, description: 'Products found' })
   @Get()
   async findAll() {
     return await this.productsService.findAll();
   }
 
+  @ApiOperation({ summary: 'Find product by ID', operationId: 'findOne' })
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({ status: 200, description: 'Product found' })
+  @ApiNotFoundResponse({ description: 'Product not found' })
   @Get(':id')
-  async findOne(id: number) {
+  async findOne(@Param('id', ParseIntPipe) id: number) {
     return await this.productsService.findOne(id);
   }
 
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiBody({ type: UpdateProductDto, description: 'Product data' })
+  @ApiResponse({ status: 200, description: 'Product updated' })
+  @ApiNotFoundResponse({ description: 'Product not found' })
   @Patch(':id')
   async update(
-    @Param('id') id: number,
+    @Param('id', ParseIntPipe) id: number,
     @Body() updateProductDto: UpdateProductDto,
   ) {
     return await this.productsService.update(id, updateProductDto);
   }
 
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({ status: 200, description: 'Product deleted' })
+  @ApiNotFoundResponse({ description: 'Product not found' })
   @Delete(':id')
-  async delete(@Param('id') id: number) {
+  async delete(@Param('id', ParseIntPipe) id: number) {
     return await this.productsService.delete(id);
   }
 }
