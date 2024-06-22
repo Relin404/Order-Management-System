c1333126d8b4f45b6f3925477d83046be4739237
relin404 <ahmadmashaal01@gmail.com>
2024-06-22 07:23:27 +0300

    chore: add module imports in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from 'src/modules/users/users.module';
+import { CartItemsModule } from './modules/cart-items/cart-items.module';
+import { OrderItemsModule } from './modules/order-items/order-items.module';
+import { ProductsModule } from 'src/modules/products/products.module';
+import { CartModule } from 'src/modules/carts/cart.module';
+import { OrdersModule } from 'src/modules/orders/orders.module';
+import { AuthModule } from 'src/modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    AuthModule,
+    CartModule,
+    CartItemsModule,
+    OrdersModule,
+    OrderItemsModule,
+    ProductsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
