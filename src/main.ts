3ef0049d4be78ac877e10cf17a1caa7e951d7546
relin404 <ahmadmashaal01@gmail.com>
2024-06-22 13:48:30 +0300

    docs: update Swagger documentation for API endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { UnknownExceptionsFilter } from 'src/common/exception-filters/unknown-exceptions.filter';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
@@ -21,19 +23,27 @@ async function bootstrap() {
   );
 
   const swaggerOptions = new DocumentBuilder()
+
     .setTitle('Slash Assessment API')
     .setDescription(
-      'Slash API for  cart management. This API is built using NestJS and uses Swagger for documentation.',
+      'Slash Order Management System API for e-commerce mobile app',
     )
     .setVersion('1.0')
-    .addServer('http://localhost:3000', 'Local development server')
+    .addServer('http://localhost:3000', 'Local Development Server')
+    .setExternalDoc(
+      'GitHub',
+      'https://github.com/Relin404/slash-assessment/blob/dev/README.md',
+    )
+    .setContact(
+      "Ahmad Mash'aal",
+      'https://www.linkedin.com/in/relin404/',
+      undefined,
+    )
     .addTag('Auth', 'Endpoints for authentication')
     .addTag('Users', 'Endpoints for user management')
     .addTag('Cart', 'Endpoints for cart management')
     .addTag('Products', 'Endpoints for product management')
     .addTag('Orders', 'Endpoints for order management')
-    .addTag('Public', 'Endpoints that are public')
-    .addTag('Protected', 'Endpoints that are protected')
     .build();
 
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
@@ -41,6 +51,10 @@ async function bootstrap() {
 
   const port = configService.get<number>('PORT');
   await app.listen(port);
+  logger.log(
+    `Swagger documentation is available at http://localhost:${port}/api/docs`,
+  );
+  logger.log(`Application is running on port ${port}`);
 }
 
 bootstrap();
