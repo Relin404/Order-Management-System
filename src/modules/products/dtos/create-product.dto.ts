28c322ebb1c27a345081ca3cf377c04d207e894b
relin404 <ahmadmashaal01@gmail.com>
2024-06-23 20:31:59 +0300

    docs: Add Swagger documentation for ProductModule DTOs

--- src/modules/products/dtos/create-product.dto.ts
+++ src/modules/products/dtos/create-product.dto.ts
@@ -1,18 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product name',
+    type: 'string',
+    example: 'T-shirt',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'Product description',
+    type: 'string',
+    example: 'A nice T-shirt',
+  })
   @IsNotEmpty()
   @IsString()
   description: string;
 
+  @ApiProperty({
+    description: 'Product price',
+    type: 'number',
+    example: 25.99,
+  })
   @IsNotEmpty()
   @IsNumber()
   price: number;
 
+  @ApiProperty({
+    description: 'Product stock',
+    type: 'number',
+    example: 10,
+  })
   @IsNotEmpty()
   @IsInt()
   stock: number;
