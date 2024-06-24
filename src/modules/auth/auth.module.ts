051d4a002bb40e4966c4d21bc56b93458172f733
relin404 <ahmadmashaal01@gmail.com>
2024-06-25 01:25:34 +0300

    feat: add JWT module with global configuration

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from 'src/modules/auth/auth.controller';
 import { AuthService } from 'src/modules/auth/auth.service';
 import { UsersModule } from 'src/modules/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: new ConfigService().get<string>('jwt.accessToken.secret'),
+      signOptions: {
+        expiresIn: new ConfigService().get<string>('jwt.accessToken.expiresIn'),
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
